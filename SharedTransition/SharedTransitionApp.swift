cd42eebcb5b42294c811738ee9fc545ca35ea323
gunnargray <hi@gunnargray.com>
2025-07-01 17:32:54 -0500

    Initial Commit

---
+++ SharedTransition/SharedTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedTransitionApp.swift
+//  SharedTransition
+//
+//  Created by Gunnar Gray on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SharedTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
